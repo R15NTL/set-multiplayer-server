15c15ae4db2adfc34858077832744f6025655df5
R15NTL <120052967+R15NTL@users.noreply.github.com>
2023-08-16 16:41:20 +0100

    User dissconnected from room if user leaves room

--- src/socket/events/handlers/common/leave-room.ts
+++ src/socket/events/handlers/common/leave-room.ts
@@ -7,12 +7,17 @@ export const leaveRoomHandler = (context: IOContext) => {
   const { socket } = context;
 
   // Validation
-  const userId = roomCache.getUserBySocketId(socket.id);
-  if (!userId) throw new Error("User is not in a room.");
+  const user = roomCache.getUserBySocketId(socket.id);
+  if (!user) throw new Error("User is not in a room.");
+
+  const roomId = roomCache.getRoomIdByUser(user.user_id);
+  if (!roomId) throw new Error("User is not in a room.");
 
   // Remove player from room
+  socket.leave(roomId);
+
   const status = removePlayerFromRoom({
-    userId: userId.user_id,
+    userId: user.user_id,
     updateLobby: true,
     updateGame: true,
     removedByHost: false,
